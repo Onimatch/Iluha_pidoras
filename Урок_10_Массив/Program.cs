0f1d96ebd9f43c7481012b80124672e3d244f85c
Onimatch <Onimatch>
2023-01-20 17:26:01 +0800

    Массивы

---
+++ Урок_10_Массив/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {1,12,4,14,15,16,4,17,21};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index]== find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
---
+++ Урок_11_Массив/Program.cs
@@ -0,0 +1,47 @@
+﻿void FillArray(int [] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection [index++] = new Random ().Next (1, 10);
+    }
+}
+
+void PrintArray (int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+      Console.WriteLine(col[position]);
+      position++;  
+    }
+}
+
+int Indexof(int[] colletction, int find)
+{
+    int count = colletction.Length;
+    int index = 0;
+    int position = -1;
+    
+
+    while (index < count)
+    {
+        if (colletction[index] == find)
+        {
+position = index;
+break;
+        }
+        index++;
+    }
+return position;
+}
+int [] array = new int [10];
+
+FillArray(array);
+PrintArray (array);
+Console.WriteLine();
+
+int pos = Indexof(array, 456);
+Console.WriteLine(pos);
